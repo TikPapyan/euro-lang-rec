bd29e8855f0114c651faf246c58f6d23f8bb6208
Enguerran-Couderc <enguerrancouderc@gmail.com>
2025-03-19 09:26:05 +0100

    renamings

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from preprocess import de_xml_dir, process_dir
-from hdc import generate_trigram_vectors, compute_language_profiles
+from hdc import generate_trigram_vectors, compute_language_vectors
 
 input_dir = 'europarl/txt'
 output_dir = 'de_xml_txt'
@@ -7,16 +7,16 @@ trigram_dir = 'trigrams'
 
 def main():
     # print("De-XMLing files...")
-    # de_xml_directory(input_dir, output_dir)
+    # de_xml_dir(input_dir, output_dir)
 
     # print("Generating trigrams...")
-    # process_directory(output_dir, trigram_dir)
+    # process_dir(output_dir, trigram_dir)
 
     # print("Preprocessing complete!")
 
     print("Computing language profiles...")
     trigram_vectors = generate_trigram_vectors('trigrams')
-    language_profiles = compute_language_profiles('trigrams')
+    language_vectors = compute_language_vectors('trigrams')
 
 if __name__ == '__main__':
     main()
